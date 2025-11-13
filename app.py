c7fa17cc05d7bbdcf6862a436a7db21ca0d917a3
maguirre2017 <32073476+maguirre2017@users.noreply.github.com>
2025-11-12 22:09:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -656,15 +656,10 @@ chart_fac_prop = (
           ),
           tooltip=["FACULTAD", "VINCULACION_PUB", "Publicaciones"]
       )
-    .properties(
-    title={
-        "text": "Distribución proporcional de publicaciones por Facultad",
-        "anchor": "start",
-        "fontSize": 16,
-        "padding": {"top": 25, "bottom": 10}   # <-- aquí revienta
-    },
-    height=350
-)
+      .properties(
+          title="Distribución proporcional de publicaciones por Facultad",
+          height=350
+      )
 )
 
 st.altair_chart(chart_fac_prop, use_container_width=True)
